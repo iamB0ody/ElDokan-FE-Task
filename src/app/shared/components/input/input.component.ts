3ee813db0261d1d7810c22beb09858059068bc79
iamB0ody <abdelrahman.ui@gmail.com>
2022-09-29 10:58:42 +0200

    ✨  Finish input reusable component

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @Input() label!: string;
+  @Input() hint!: string;
+  @Input() placeholder!: string;
+  @Input() type!: string;
+  @Input() classes: string = 'form-control';
+  @Input() containerclasses: string = 'form-group';
+  @Input() ids!: string;
+  @Input() srOnly!: string;
+  @Input() options!: string[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
