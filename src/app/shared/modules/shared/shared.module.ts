d61b090ee1144f67ef01f6010fb56126e1371959
iamB0ody <abdelrahman.ui@gmail.com>
2022-09-30 18:43:36 +0200

    ✨  Create toast component

--- src/app/shared/modules/shared/shared.module.ts
+++ src/app/shared/modules/shared/shared.module.ts
@@ -1,11 +1,21 @@
+import { ToastComponent } from './../../components/toast/toast.component';
+import { ModalComponent } from './../../components/modal/modal.component';
 import { InputComponent } from './../../components/input/input.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModalModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [InputComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [InputComponent, ReactiveFormsModule],
+  declarations: [InputComponent, ModalComponent, ToastComponent],
+  imports: [CommonModule, ReactiveFormsModule, NgbModalModule, NgbToastModule],
+  exports: [
+    InputComponent,
+    ReactiveFormsModule,
+    NgbModalModule,
+    ModalComponent,
+    ToastComponent,
+    NgbToastModule,
+  ],
 })
 export class SharedModule {}
